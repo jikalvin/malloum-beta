3b574aa4321b51ca6805641bbdc0ddf932721326
unknown <pamelafugua@gmail.com>
2023-04-25 16:06:20 +0200

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 const API_KEY = "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";
-// "Explain things like you would to a 10 year old learning how to code."
-const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
+
+const systemMessage = { 
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
 }
 
@@ -36,10 +36,7 @@ function App() {
     await processMessageToChatGPT(newMessages);
   };
 
-  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
-    // Format messages for chatGPT API
-    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-    // So we need to reformat
+  async function processMessageToChatGPT(chatMessages) { 
 
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
@@ -52,14 +49,12 @@ function App() {
     });
 
 
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want chatGPT to act. 
+   
     const apiRequestBody = {
       "model": "gpt-3.5-turbo",
       "messages": [
-        systemMessage,  // The system message DEFINES the logic of our chatGPT
-        ...apiMessages // The messages from our chat with ChatGPT
+        systemMessage,  
+        ...apiMessages
       ]
     }
 
